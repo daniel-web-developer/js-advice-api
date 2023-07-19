407c5e33b550fb6a2577f07bf0fa625a14d35e57
daniel-web-developer <daniel_caixeta@protonmail.com>
2023-07-19 22:52:11 +0200

    all done

--- README.md
+++ README.md
@@ -91,3 +91,5 @@ We love receiving feedback! We're always looking to improve our challenges and o
 This challenge is completely free. Please share it with anyone who will find it useful for practice.
 
 **Have fun building!** 🚀
+
+https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
--- script.js
+++ script.js
@@ -7,7 +7,9 @@ const advice = document.querySelector('#advice');
 const url = "https://api.adviceslip.com/advice"
 
 async function getAdvices(){
-    const response = await fetch(url);
+    const response = await fetch(url, {
+        cache: "no-cache"
+    });
     const data = await response.json();
     number.innerHTML = "ADVICE #" + (data.slip.id + 1);
     advice.innerHTML = "\"" + data.slip.advice + "\"";
