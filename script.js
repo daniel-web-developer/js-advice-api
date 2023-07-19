66bc9fb32687915b4fdef74bfe70217b4d175cce
daniel-web-developer <daniel_caixeta@protonmail.com>
2023-07-19 22:49:17 +0200

    done, but api isn't working properly

---
+++ script.js
@@ -0,0 +1,20 @@
+console.log("VIVA CRISTO REY");
+
+const button = document.querySelector('#button');
+const number = document.querySelector('#advice-number');
+const advice = document.querySelector('#advice');
+
+const url = "https://api.adviceslip.com/advice"
+
+async function getAdvices(){
+    const response = await fetch(url);
+    const data = await response.json();
+    number.innerHTML = "ADVICE #" + (data.slip.id + 1);
+    advice.innerHTML = "\"" + data.slip.advice + "\"";
+}
+
+getAdvices();
+
+button.addEventListener('click', () => {
+    getAdvices();
+})
